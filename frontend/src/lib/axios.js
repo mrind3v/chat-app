7e4a6b9ecad1afdef2dd164fc49e817c7d1280e8
mrind3v <mrinmaydev@outlook.com>
2025-04-21 10:37:15 +0530

    made some changes regarding .env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // axios helps us making HTTP requests from React apps
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  //baseURL: "http://localhost:5001/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-xy1m.onrender.com/api",
   withCredentials: true, // send cookies in every single HTTP request 
 });
